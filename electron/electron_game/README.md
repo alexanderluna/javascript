a762936e94cd65bf5d3ad8b23737549f99ff24b8
Mycroft1891 <mycroft1891@gmail.com>
2016-07-10 22:15:55 -0500

    Add objects and light to scene

---
+++ electron/electron_game/README.md
@@ -0,0 +1,3 @@
+#Electron Game
+
+This is a simple desktop game build with the Babylon js game engine and Electron. 
--- electron/electron_game/script/app.js
+++ electron/electron_game/script/app.js
@@ -8,9 +8,19 @@ function startBabylonJS() {
   if (BABYLON.Engine.isSupported()){
     canvas = document.getElementById("renderCanvas");
     engine = new BABYLON.Engine(canvas, true);
+    scene = new BABYLON.Scene(engine);
+
+    var cam = new BABYLON.FreeCamera("freecam", new BABYLON.Vector3(0, 0, -10), scene);
+    cam.attachControl(canvas);
+
+    var cube = BABYLON.Mesh.CreateBox("cube", 2, scene);
+    var light = new BABYLON.PointLight("plight", new BABYLON.Vector3(5, 10, -4), scene);
+
 
     engine.runRenderLoop(function(){
-      engine.clear(new BABYLON.Color3(0.2, 0.2, 0.3), true);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      scene.render();
     });
   }
 }
