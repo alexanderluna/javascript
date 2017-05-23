08852508d168e1eb6aac37e6a0baefe31ec81d32
Mycroft1891 <mycroft1891@gmail.com>
2017-05-23 02:40:43 -0500

    Finish up Angular Project

--- angular/my-first-app/src/app/server/server.component.ts
+++ angular/my-first-app/src/app/server/server.component.ts
@@ -5,5 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  serverID: number = 10;
+  serverStatus: string = "offline";
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'offline' : 'online';
+  }
+
+  getClass() {
+    return this.serverStatus === 'offline' ? 'alert alert-warning' : 'alert alert-success';
+  }
 }
