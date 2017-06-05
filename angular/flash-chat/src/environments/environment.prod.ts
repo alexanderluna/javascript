90f0c285582e7a88f27e3ef6ebafdb4d20913919
Mycroft1891 <mycroft1891@gmail.com>
2017-06-05 01:15:37 -0500

    Save Final changes for production

--- angular/flash-chat/src/environments/environment.prod.ts
+++ angular/flash-chat/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBjF3WAsiqHUKfio1qXG0xkbNJGMRCzAX0',
+    authDomain: 'flash-chat-ca9f8.firebaseapp.com',
+    databaseURL: 'https://flash-chat-ca9f8.firebaseio.com',
+    projectId: 'flash-chat-ca9f8',
+    storageBucket: 'flash-chat-ca9f8.appspot.com',
+    messagingSenderId: '661747648952'
+  }
 };
