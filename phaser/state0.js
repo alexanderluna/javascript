f9d25ce06d6a9f1179661e16608808c5a9b41afc
Mycroft1891 <mycroft1891@gmail.com>
2017-06-27 23:36:29 -0500

    Add animation and motion to game scenes

--- phaser/state0.js
+++ phaser/state0.js
@@ -1,27 +1,71 @@
-var demo = {};
+// declare global variables
+var demo = {}, centerX = 1500/2, centerY = 1000/2, player, speed = 10 ;
+
+// create state
 demo.state0 = function(){};
+
+// define state functions
 demo.state0.prototype = {
   preload: function() {
-    game.load.image('player', 'assets/player.png');
+    //game.load.image('player', 'assets/player-one.png');
+    game.load.spritesheet('player','assets/player-spritesheet.png',127,208);
+    game.load.image('tree', 'assets/background.png');
   },
   create: function(){
+    // config scene
+    console.log("State 0");
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.stage.backgroundColor = '#80FF80';
-    console.log("State 0");
+    game.world.setBounds(0,0,2000,2000);
+    game.camera.deadzone = new Phaser.Rectangle(centerX - 300, 0, 600, 1000);
     addStateEventListeners();
-    game.add.sprite(1500/2, 1000/2, 'player');
+
+    // config spites
+    var bgImg =  game.add.sprite(0, 0, 'tree');
+    player = game.add.sprite(centerX, centerY, 'player');
+    player.anchor.setTo(0.5,0.5);
+    player.scale.setTo(0.7,0.7);
+    game.physics.enable(player);
+    player.body.collideWorldBounds = true;
+    game.camera.follow(player);
   },
-  update: function(){}
+  update: function(){
+    // responde to user input
+    if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+      player.scale.setTo(0.7,0.7);
+      player.x += speed;
+      player.angle = 15;
+    } else if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+      player.scale.setTo(-0.7,0.7);
+      player.x -= speed;
+      player.angle = -15;
+    } else if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+      player.y -= speed;
+      if (player.y < 855) {
+        player.y = 855;
+      }
+    } else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+      player.y += speed;
+    } else {
+      player.rotation = 0;
+    }
+  }
 };
 
+// define global functions //
+
+// change state based on key pressed
 function changeState(i, key) {
   game.state.start('state'+key)
 }
 
+// key down event listener
 function addKeyCallback(key,args){
   game.input.keyboard.addKey(key).onDown.add(changeState, null, null, args);
 }
 
+// call custom callback for key pressed
 function addStateEventListeners(){
   addKeyCallback(Phaser.Keyboard.ZERO, 0);
   addKeyCallback(Phaser.Keyboard.ONE, 1);
