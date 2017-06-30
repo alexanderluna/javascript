db68137020f10420bb3277d413089e22a5298645
Mycroft1891 <mycroft1891@gmail.com>
2017-06-29 22:59:29 -0500

    Save State1 progress

--- phaser/state1.js
+++ phaser/state1.js
@@ -1,9 +1,11 @@
 demo.state1 = function() {};
+var cursors, vel = 500;
 demo.state1.prototype = {
   preload: function() {
     game.load.tilemap('field', 'assets/field2.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('grassTiles', 'assets/grassTiles.png');
     game.load.image('rockTiles', 'assets/rockTiles.png');
+    game.load.spritesheet('player', 'assets/player.png');
   },
   create: function() {
     game.stage.backgroundColor = '#4c76e1';
@@ -15,8 +17,16 @@ demo.state1.prototype = {
 
     var grass = map.createLayer('grass');
     var rocks = map.createLayer('rocks');
+
+    player = game.add.sprite(200,200,'player');
+    player.scale.setTo(0.2,0.2);
+    game.physics.enable(player);
+
+    cursors = game.input.keyboard.createCursorKeys();
   },
   update: function() {
+    if (cursors.up.isDown) {
 
+    }
   }
 }
