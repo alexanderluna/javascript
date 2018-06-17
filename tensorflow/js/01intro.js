6cd8b2829ae33885e2d7a73c017538586aef95e9
Mycroft1891 <mycroft1891@gmail.com>
2018-06-17 23:46:48 +0200

    Add new Tensorflow files

---
+++ tensorflow/js/01intro.js
@@ -0,0 +1,17 @@
+console.log("***** 01 Intro *****");
+
+//Let TensorFlow clean up after itself using .tidy()
+tf.tidy(() => {
+  let numberArray = [];
+  for (var i = 0; i < 15 ; i++) {
+    numberArray[i] = Math.random() * 100;
+  }
+  const shapeA = [5,3];
+  const shapeB = [3,5];
+
+  const a = tf.tensor(numberArray, shapeA, "int32");
+  const b = tf.tensor(numberArray, shapeB, "int32");
+  const c = a.matMul(b);
+  c.print();
+
+})
---
+++ tensorflow/js/02sequential.js
@@ -0,0 +1,10 @@
+console.log("***** 02 Sequential *****");
+const model = tf.sequential();
+const hidden = tf.layers.dense({units: 4, inputShape: [2], activation: "sigmoid"});
+const output = tf.layers.dense({units: 3, activation: "sigmoid"});
+
+model.add(hidden);
+model.add(output);
+
+model.compile({optimizer: tf.train.sgd(0.1), loss: "meanSquaredError"});
+
