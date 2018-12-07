c418a0e2f9d7e753c76eb78bdb2e6c305548125e
Mycroft1891 <mycroft1891@gmail.com>
2018-12-07 22:44:41 +0100

    Add: blog setup using redux and redux thunk

---
+++ react/blog/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import App from './components/App';
+import reducers from './reducers';
+
+ReactDOM.render(
+    <Provider store={createStore(reducers, applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
