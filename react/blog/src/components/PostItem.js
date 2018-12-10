09eeb54ca165dc979aa5ee3170a5e94379656262
Mycroft1891 <mycroft1891@gmail.com>
2018-12-10 22:47:26 +0100

    Add: blueprint files for the rest of the project

---
+++ react/blog/src/components/PostItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class PostItem extends React.Component {
+    render() {
+        return (
+            <div>
+                PostItem
+            </div>
+        )
+    }
+}
+
+export default PostItem;
