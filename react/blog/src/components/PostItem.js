e62dec2bbe8d4e2c7176bd81558f568ea4a4f537
Mycroft1891 <mycroft1891@gmail.com>
2018-12-15 13:12:04 +0100

    Update: render posts individually using own component

--- react/blog/src/components/PostItem.js
+++ react/blog/src/components/PostItem.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import UserHeader from './UserHeader';
 
 class PostItem extends React.Component {
     render() {
+        const { post } = this.props;
         return (
-            <div>
-                PostItem
+            <div className="item">
+                <i className="large middle aligned icon user" />
+                <div className="content">
+                    <div className="description">
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                    </div>
+                    <UserHeader userId={post.userId} />
+                </div>
             </div>
         )
     }
