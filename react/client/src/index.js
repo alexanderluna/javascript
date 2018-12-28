470017758ea533711265a9ebd224e12d32d17d83
Mycroft1891 <mycroft1891@gmail.com>
2018-12-28 19:08:58 +0100

    Update: use redux thunk for google authentication

--- react/client/src/index.js
+++ react/client/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from "react-redux";
 import reducers from "./reducers";
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
-    <Provider store={createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+    <Provider store={store}>
         <App />
     </Provider>,
     document.getElementById('root')
