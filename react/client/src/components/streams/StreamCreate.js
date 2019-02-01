3e505dbdd3b64e0230c1aa50b763f8c579b3d93d
Mycroft1891 <mycroft1891@gmail.com>
2019-02-01 18:34:30 +0100

    Update: move redux form into own component for reuse

--- react/client/src/components/streams/StreamCreate.js
+++ react/client/src/components/streams/StreamCreate.js
@@ -1,74 +1,26 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { createStream } from '../../actions';
-import ErrorMessage from '../ErrorMessage';
-
-const required = field => value => (
-  value
-    ? undefined
-    : `${field} can't be blank`
-);
-const minLength = min => value => (
-  value && value.length < min
-    ? `Must be at least ${min} characters`
-    : undefined
-);
-const minLength10 = minLength(10);
-const minLength20 = minLength(20);
-const requiredTitle = required('title');
-const requiredDescription = required('description');
+import StreamForm from './partials/StreamForm';
 
 class StreamCreate extends Component {
-  renderInput = ({ input, label, meta: { touched, error } }) => {
-    const className = `field ${touched && error ? 'error' : ''}`;
-    return (
-      <div className={className}>
-        <label htmlFor="title" type="text">{label}</label>
-        <input id="title" type="text" {...input} autoComplete="off" />
-        {touched && error && <ErrorMessage>{error}</ErrorMessage>}
-      </div>
-    );
-  }
-
   onSubmit = (formProps) => {
     this.props.createStream(formProps);
   }
 
   render() {
-    const { handleSubmit } = this.props;
     return (
-      <form
-        name="create-form"
-        className="ui form error"
-        onSubmit={handleSubmit(this.onSubmit)}
-      >
-        <Field
-          name="title"
-          component={this.renderInput}
-          label="title"
-          validate={[requiredTitle, minLength10]}
-        />
-        <Field
-          name="description"
-          component={this.renderInput}
-          label="description"
-          validate={[requiredDescription, minLength20]}
-        />
-        <button type="submit" className="ui button primary">Submit</button>
-      </form>
+      <div className="">
+        <h3>Create Stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
     );
   }
 }
 
 StreamCreate.propTypes = {
-  handleSubmit: PropTypes.func.isRequired,
   createStream: PropTypes.func.isRequired,
 };
 
-const formWrapped = reduxForm({
-  form: 'streamCreate',
-})(StreamCreate);
-
-export default connect(null, { createStream })(formWrapped);
+export default connect(null, { createStream })(StreamCreate);
---
+++ react/client/src/components/streams/partials/StreamForm.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import PropTypes from 'prop-types';
+import ErrorMessage from '../../ErrorMessage';
+import {
+  requiredTitle,
+  requiredDescription,
+  minLength10,
+  minLength20,
+} from '../validators';
+
+class StreamForm extends Component {
+  renderInput = ({ input, label, meta: { touched, error } }) => {
+    const className = `field ${touched && error ? 'error' : ''}`;
+    return (
+      <div className={className}>
+        <label htmlFor="title" type="text">{label}</label>
+        <input id="title" type="text" {...input} autoComplete="off" />
+        {touched && error && <ErrorMessage>{error}</ErrorMessage>}
+      </div>
+    );
+  }
+
+  render() {
+    const { handleSubmit, onSubmit } = this.props;
+    return (
+      <form
+        name="create-form"
+        className="ui form error"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <Field
+          name="title"
+          component={this.renderInput}
+          label="title"
+          validate={[requiredTitle, minLength10]}
+        />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="description"
+          validate={[requiredDescription, minLength20]}
+        />
+        <button type="submit" className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+StreamForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default reduxForm({ form: 'streamForm' })(StreamForm);
