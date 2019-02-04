070653b5978ae43fa8b54f1ff296a028a82a49bc
Mycroft1891 <mycroft1891@gmail.com>
2019-02-05 00:43:54 +0100

    Update: implement stream delete functionality

--- react/client/src/components/streams/StreamDelete.js
+++ react/client/src/components/streams/StreamDelete.js
@@ -1,13 +1,87 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import history from '../../history';
+import { fetchStream, deleteStream } from '../../actions';
+import Modal from './partials/Modal';
 
 class StreamDelete extends Component {
-  render() {
+  componentDidMount() {
+    const { fetchStream, match: { params } } = this.props;
+    fetchStream(params.id);
+  }
+
+  get actions() {
+    const { deleteStream, match: { params } } = this.props;
     return (
       <div>
-        Delete
+        <button
+          type="button"
+          className="ui button negative"
+          onClick={() => deleteStream(params.id)}
+        >
+          Delete
+        </button>
+        <button
+          type="button"
+          className="ui button"
+          onClick={() => history.push('/')}
+        >
+          Cancel
+        </button>
+      </div>
+    );
+  }
+
+  get contentText() {
+    const { stream } = this.props;
+    return (
+      `Are you sure you want to delete the stream:
+      "${stream.title.toUpperCase()}"`
+    );
+  }
+
+  render() {
+    const { stream } = this.props;
+    return (
+      <div>
+        {!stream && <h2>Loading...</h2>}
+        {stream && (
+          <Modal
+            title="Delete Stream"
+            content={this.contentText}
+            actions={this.actions}
+            onDismiss={() => history.push('/')}
+          />
+        )}
       </div>
     );
   }
 }
 
-export default StreamDelete;
+StreamDelete.propTypes = {
+  fetchStream: PropTypes.func.isRequired,
+  deleteStream: PropTypes.func.isRequired,
+  stream: PropTypes.objectOf(PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ])),
+  match: PropTypes.objectOf(PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object,
+    PropTypes.bool,
+  ])).isRequired,
+};
+
+StreamDelete.defaultProps = {
+  stream: null,
+};
+
+const mapStateToProps = (state, ownProps) => (
+  { stream: state.streams[ownProps.match.params.id] }
+);
+
+export default connect(
+  mapStateToProps,
+  { fetchStream, deleteStream },
+)(StreamDelete);
