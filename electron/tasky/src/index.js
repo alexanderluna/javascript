6f65ca2adf863bf050c6eeb0cfbc9af3499cdba7
Mycroft1891 <mycroft1891@gmail.com>
2019-05-05 21:47:34 +0200

    Update: cleanup and follow eslint style guide

--- electron/tasky/src/index.js
+++ electron/tasky/src/index.js
@@ -1,16 +1,11 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import { HashRouter as Router, Route } from "react-router-dom";
-
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import App from './components/App';
 
 ReactDOM.render(
   <Router>
-    <Route
-      component={() => (
-        <App />
-      )}
-    />
+    <Route component={App} />
   </Router>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
