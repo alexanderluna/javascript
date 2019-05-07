7ce158a869f1b333201e9253a3c12ef462f04343
Mycroft1891 <mycroft1891@gmail.com>
2019-05-07 22:09:33 +0200

    Update: set window size, remove frame and make it fixed sized

--- electron/tasky/index.js
+++ electron/tasky/index.js
@@ -3,7 +3,12 @@ const { app, BrowserWindow } = require('electron');
 let mainWindow;
 
 function createMainWindow() {
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+    height: 500,
+    width: 300,
+    frame: false,
+    resizable: false,
+  });
   mainWindow.loadURL('http://localhost:3000');
 }
 
