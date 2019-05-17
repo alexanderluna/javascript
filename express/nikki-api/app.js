983aaa5f9faca788eb20f8099df686d920531e37
Mycroft1891 <mycroft1891@gmail.com>
2019-05-17 13:09:01 +0200

    Update: setup connection to mongodb

--- express/nikki-api/app.js
+++ express/nikki-api/app.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import config from './config';
 import entries from './routes/api/entries';
 
 const app = express();
+
+(async () => {
+  const db = await mongoose.connect(config.DB_URL, { useNewUrlParser: true });
+  console.log(`☁️ Database connected: ${db.connection.name}`);
+})();
 
 app.use('/api/entries', entries);
 
