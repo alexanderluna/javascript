e7eb630a141926d148c23c97a2636ec666a7b089
Mycroft1891 <mycroft1891@gmail.com>
2019-06-02 10:33:28 +0200

    Update: handle video metadata fetching with promises

--- electron/convert/index.js
+++ electron/convert/index.js
@@ -1,10 +1,14 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+const _ = require('lodash');
 
 app.commandLine.appendSwitch('disable-renderer-backgrounding');
 app.commandLine.appendSwitch('disable-background-timer-throttling');
 
+let mainWindow;
+
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
     webPreferences: {
@@ -14,4 +18,24 @@ app.on('ready', () => {
   });
 
   mainWindow.loadURL('http://localhost:3000');
+});
+
+ipcMain.on('videosAdded', (event, videos) => {
+  const promises = _.map(videos, (video) => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        if (err) reject(err);
+        resolve({
+          ...video,
+          duration: metadata.format.duration,
+          format: 'avi'
+        });
+      });
+    });
+  });
+
+  Promise.all(promises)
+    .then((results) => {
+      mainWindow.webContents.send('metadataComplete', results)
+    });
 });
