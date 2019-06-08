5b74b86b38fe81945f50fcb31781e0b7264c6524
Mycroft1891 <mycroft1891@gmail.com>
2019-06-08 21:36:38 +0200

    Update: actions to fetch video metadata, convert list of videos and open window for file

--- electron/convert/src/actions/index.js
+++ electron/convert/src/actions/index.js
@@ -1,51 +1,50 @@
-import { ADD_VIDEO, ADD_VIDEOS, REMOVE_VIDEO, REMOVE_ALL_VIDEOS, VIDEO_PROGRESS, VIDEO_COMPLETE } from "./types";
-const { ipcRenderer } = window.require("electron");
+import {
+  ADD_VIDEO,
+  ADD_VIDEOS,
+  REMOVE_VIDEO,
+  REMOVE_ALL_VIDEOS,
+  VIDEO_PROGRESS,
+  VIDEO_COMPLETE,
+} from './types';
 
-export const addVideos = videos => dispatch => {
+const { ipcRenderer } = window.require('electron');
+
+export const addVideos = (videos) => (dispatch) => {
   ipcRenderer.send('videosAdded', videos);
   ipcRenderer.on('metadataComplete', (event, results) => {
     dispatch({ type: ADD_VIDEOS, payload: results });
   });
 };
 
-
-// TODO: Communicate to MainWindow that the user wants
-// to start converting videos.  Also listen for feedback
-// from the MainWindow regarding the current state of
-// conversion.
 export const convertVideos = () => (dispatch, getState) => {
-
+  ipcRenderer.send('conversionStart', getState().videos);
+  ipcRenderer.on('conversionEnd', (event, { video, outputPath }) => {
+    dispatch({ type: VIDEO_COMPLETE, payload: { ...video, outputPath } });
+  });
+  ipcRenderer.on('conversionProgress', (event, { video, timemark }) => {
+    dispatch({ type: VIDEO_PROGRESS, payload: { ...video, timemark } });
+  });
 };
 
-// TODO: Open the folder that the newly created video
-// exists in
-export const showInFolder = outputPath => dispatch => {
-
+export const showInFolder = (outputPath) => () => {
+  ipcRenderer.send('folderOpen', outputPath);
 };
 
-export const addVideo = video => {
-  return {
-    type: ADD_VIDEO,
-    payload: { ...video }
-  };
-};
+export const addVideo = (video) => ({
+  type: ADD_VIDEO,
+  payload: { ...video },
+});
 
-export const setFormat = (video, format) => {
-  return {
-    type: ADD_VIDEO,
-    payload: { ...video, format, err: "" }
-  };
-};
+export const setFormat = (video, format) => ({
+  type: ADD_VIDEO,
+  payload: { ...video, format, err: '' },
+});
 
-export const removeVideo = video => {
-  return {
-    type: REMOVE_VIDEO,
-    payload: video
-  };
-};
+export const removeVideo = (video) => ({
+  type: REMOVE_VIDEO,
+  payload: video,
+});
 
-export const removeAllVideos = () => {
-  return {
-    type: REMOVE_ALL_VIDEOS
-  };
-};
+export const removeAllVideos = () => ({
+  type: REMOVE_ALL_VIDEOS,
+});
