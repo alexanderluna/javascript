8e1d29b4893abc903aaa6339178849d18824318d
Mycroft1891 <mycroft1891@gmail.com>
2019-06-11 21:39:01 +0200

    Update: convert to functional component and use newer syntax

--- electron/convert/src/screens/ConvertScreen.js
+++ electron/convert/src/screens/ConvertScreen.js
@@ -1,30 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import VideoList from '../components/VideoList';
 import ConvertPanel from '../components/ConvertPanel';
 import VideoSelectScreen from './VideoSelectScreen';
 import { setFormat, removeVideo, showInFolder } from '../actions';
 
-class ConvertScreen extends Component {
-  render() {
-    return (
-      <div className="container">
-        <VideoSelectScreen small />
-        <VideoList
-          videos={this.props.videos}
-          onFormatChange={this.props.setFormat}
-          onFolderOpen={this.props.showInFolder}
-          removeVideo={this.props.removeVideo}
+const ConvertScreen = (props) => (
+  <div className="container">
+    <VideoSelectScreen small />
+    <VideoList
+      videos={props.videos}
+      onFormatChange={props.setFormat}
+      onFolderOpen={props.showInFolder}
+      removeVideo={props.removeVideo}
 
-        />
-        <ConvertPanel />
-      </div>
-    );
-  }
-}
+    />
+    <ConvertPanel />
+  </div>
+);
 
-function mapStateToProps(state) {
-  return { videos: state.videos };
-}
+const mapStateToProps = ({ videos }) => ({ videos });
 
-export default connect(mapStateToProps, { setFormat, removeVideo, showInFolder })(ConvertScreen);
+export default connect(
+  mapStateToProps,
+  { setFormat, removeVideo, showInFolder },
+)(ConvertScreen);
