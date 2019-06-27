e52736268f3ed9236ee8aee18dbc2555d175b9e0
Mycroft1891 <mycroft1891@gmail.com>
2019-06-28 00:59:12 +0200

    Update: google oauth2.0 authentication

--- express/react-server/index.js
+++ express/react-server/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ hello: 'there' });
-});
+passport.use(new GoogleStrategy({
+  clientID: keys.google.clientID,
+  clientSecret: keys.google.clientSecret,
+  callbackURL: keys.google.callBackUrl,
+}, (accessToken, refreshToken, profile) => {
+  console.log('access token: ', accessToken);
+  console.log('refresh token: ', refreshToken);
+  console.log('profile: ', profile);
+}));
 
-const PORT = process.env.PORT || 5000;
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT);
