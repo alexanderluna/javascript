322fe2753120d1e9fe1bf97dc2741382ff6c6afe
Mycroft1891 <mycroft1891@gmail.com>
2019-07-19 22:51:08 +0200

    Update: mongoose connection and schema setup

--- express/react-server/index.js
+++ express/react-server/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 const authRoutes = require('./routes/authRoutes');
+
+mongoose.connect(
+  keys.mongoURI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
 
 const app = express();
 
