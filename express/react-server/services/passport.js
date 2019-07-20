0726f8813139d9219bfac716a3a7e0cdec33966e
Mycroft1891 <mycroft1891@gmail.com>
2019-07-20 22:51:43 +0200

    Update: configure passport to load user schema and create user

--- express/react-server/services/passport.js
+++ express/react-server/services/passport.js
@@ -1,13 +1,14 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
   clientID: keys.google.clientID,
   clientSecret: keys.google.clientSecret,
   callbackURL: keys.google.callBackUrl,
 }, (accessToken, refreshToken, profile) => {
-  console.log('access token: ', accessToken);
-  console.log('refresh token: ', refreshToken);
-  console.log('profile: ', profile);
+  new User({ googleId: profile.id }).save();
 }));
