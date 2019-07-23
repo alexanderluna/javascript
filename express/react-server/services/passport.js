33152578dad5f608f56d2ede8ffb94c3b119903e
Mycroft1891 <mycroft1891@gmail.com>
2019-07-23 18:23:19 +0200

    Update: serialize user and add logic to avoid duplicate users

--- express/react-server/services/passport.js
+++ express/react-server/services/passport.js
@@ -5,10 +5,23 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
 passport.use(new GoogleStrategy({
   clientID: keys.google.clientID,
   clientSecret: keys.google.clientSecret,
   callbackURL: keys.google.callBackUrl,
-}, (accessToken, refreshToken, profile) => {
-  new User({ googleId: profile.id }).save();
+}, (accessToken, refreshToken, profile, done) => {
+  User.findOne({ googleId: profile.id })
+    .then((existingUser) => {
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        new User({ googleId: profile.id })
+          .save()
+          .then((user) => done(null, user));
+      }
+    });
 }));
