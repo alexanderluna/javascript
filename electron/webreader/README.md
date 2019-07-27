b5882984f1126cb415d0c48036c06c99d676157e
Mycroft1891 <mycroft1891@gmail.com>
2019-07-27 23:08:40 +0200

    Init project

---
+++ electron/webreader/README.md
@@ -0,0 +1,5 @@
+# Web Reader
+
+This is an electron app which allows the user to add a web link which will be
+stored inside the app so the user can access the website later just like a
+bookmark.
---
+++ electron/webreader/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile('renderer/main.html');
+  mainWindow.webContents.openDevTools();
+  mainWindow.on('close', () => {
+    mainWindow = null;
+  });
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
