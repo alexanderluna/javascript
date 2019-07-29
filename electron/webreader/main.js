5e4d053080dcb8fadcdfec7887149f9f17f1d589
Mycroft1891 <mycroft1891@gmail.com>
2019-07-29 23:56:56 +0200

    Update: mainWindow configuration with state

--- electron/webreader/main.js
+++ electron/webreader/main.js
@@ -1,18 +1,32 @@
 const { app, BrowserWindow } = require('electron');
+const windowStateKeeper = require('electron-window-state');
 
 let mainWindow;
 
 const createWindow = () => {
+  let state = windowStateKeeper({
+    defaultWidth: 500,
+    defaultHeight: 650,
+  });
+
   mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 800,
+    x: state.x,
+    y: state.y,
+    width: state.width,
+    height: state.height,
+    minWidth: 350,
+    maxWidth: 650,
+    minHeight: 300,
     webPreferences: {
       nodeIntegration: true,
     },
   });
 
+  state.manage(mainWindow);
+
   mainWindow.loadFile('renderer/main.html');
   mainWindow.webContents.openDevTools();
+
   mainWindow.on('close', () => {
     mainWindow = null;
   });
