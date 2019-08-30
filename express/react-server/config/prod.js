6d4d8142f4bfbcd129c189935f7882dc80bed259
Mycroft1891 <mycroft1891@gmail.com>
2019-08-31 01:05:22 +0200

    Add: conifguration files for production environment

---
+++ express/react-server/config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  google: {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callBackUrl: process.env.GOOGLE_CALLBACK_URI,
+  },
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
