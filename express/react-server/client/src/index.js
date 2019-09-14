212f7ec79257198b8a9ad58f2c092146f8ba2548
Mycroft1891 <mycroft1891@gmail.com>
2019-09-14 15:17:16 +0200

    Update: redux store setup

--- express/react-server/client/src/index.js
+++ express/react-server/client/src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import * as serviceWorker from './serviceWorker';
+import App from './components/App';
+import reducers from './reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
