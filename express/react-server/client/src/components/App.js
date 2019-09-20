dec7cd7c18f441705dc2d069c1f83127336f429d
Mycroft1891 <mycroft1891@gmail.com>
2019-09-20 23:38:42 +0200

    Add: styling and react hook for lifecycle method

--- express/react-server/client/src/components/App.js
+++ express/react-server/client/src/components/App.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+import './App.css';
+import Header from './Header';
 
-const Header = () => (<h2>Header</h2>);
 const Dashboard = () => (<h2>Dashboard</h2>);
 const SurveyNew = () => (<h2>SurveyNew</h2>);
 const Landing = () => (<h2>Landing</h2>);
 
-const App = () => (
-  <div>
-    <BrowserRouter>
-      <div>
-        <Header />
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/surveys" component={Dashboard} />
-        <Route exact path="/surveys/new" component={SurveyNew} />
-      </div>
-    </BrowserRouter>
-  </div>
-);
+const App = (props) => {
 
-export default App;
+  useEffect(() => {
+    props.fetchUser();
+  });
+
+  return (
+    <div className="container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route exact path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  )
+};
+
+export default connect(null, actions)(App);
