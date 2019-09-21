ea429ec8a12ee0f2e4149ab97589d1c987023fdd
Mycroft1891 <mycroft1891@gmail.com>
2019-09-21 23:40:35 +0200

    Add: Header component with styling

---
+++ express/react-server/client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => (
+  <div className="navigation">
+    <h1>Emaily</h1>
+    <ul>
+      <li>
+        <a href="/dummy">Login with Google</a>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Header;
