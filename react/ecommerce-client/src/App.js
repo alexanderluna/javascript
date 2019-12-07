0b6f5f21bde580c0a81eabadf5256e5ef1681255
Mycroft1891 <mycroft1891@gmail.com>
2019-12-07 21:16:43 +0100

    Add: Homepage component and styling

---
+++ react/ecommerce-client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Homepage from "./pages/Homepage";
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
