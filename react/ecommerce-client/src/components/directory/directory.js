1c4d2591951d2cffdb6d105213f3c4d7ee1dbc73
Mycroft1891 <mycroft1891@gmail.com>
2019-12-08 21:18:05 +0100

    Add: Directory component with useState hook to feed the menu-item component

---
+++ react/ecommerce-client/src/components/directory/directory.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import MenuItem from '../menu-item/menu-item';
+import './directory.sass';
+
+const Directory = () => {
+  const [sections] = useState([
+    { title: 'title1', imgUrl: 'https://via.placeholder.com/150', id: 1 },
+    { title: 'title2', imgUrl: 'https://via.placeholder.com/150', id: 2 },
+    { title: 'title3', imgUrl: 'https://via.placeholder.com/150', id: 3 },
+    { title: 'title4', imgUrl: 'https://via.placeholder.com/150', id: 4 },
+    { title: 'title5', imgUrl: 'https://via.placeholder.com/150', id: 5 },
+  ]);
+
+  return (
+    <div className="directory-menu">
+      {sections.map(section => (
+        <MenuItem key={section.id} {...section} />
+      ))}
+    </div>
+  )
+}
+
+export default Directory;
