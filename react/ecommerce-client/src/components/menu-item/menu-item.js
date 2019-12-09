b8f40fd62a683caeb7a82851de93cf812039d6b0
Mycroft1891 <mycroft1891@gmail.com>
2019-12-09 21:19:13 +0100

    Add: MenuItem component to render individual items and extensive styling

---
+++ react/ecommerce-client/src/components/menu-item/menu-item.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './menu-item.sass';
+
+const MenuItem = ({ title, imgUrl }) => {
+  return (
+    <div className="menu-item">
+      <div
+        className="background-img"
+        style={{ backgroundImage: `url(${imgUrl})` }}
+      />
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">subtitle</span>
+      </div>
+    </div>
+  )
+}
+
+export default MenuItem;
