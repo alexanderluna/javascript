bd20869c2f8bf5951715c558e61890fbd1c665ae
Mycroft1891 <mycroft1891@gmail.com>
2020-02-22 23:03:37 +0100

    Add: collection item component with sass styling

--- react/ecommerce-client/src/components/preview-collection/preview-collection.js
+++ react/ecommerce-client/src/components/preview-collection/preview-collection.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import CollectionItem from '../collection-item/collection-item';
+import './preview-collection.sass';
 
 const PreviewCollection = ({ title, items }) => (
   <div className="preview-collection">
@@ -6,8 +8,13 @@ const PreviewCollection = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((_, index) => index < 4)
-        .map(({ id, name }) => (
-          <div key={id}>{name}</div>
+        .map(({ id, name, price, imageUrl }) => (
+          <CollectionItem
+            key={id}
+            name={name}
+            price={price}
+            imageUrl={imageUrl}
+          />
         ))}
     </div>
   </div>
