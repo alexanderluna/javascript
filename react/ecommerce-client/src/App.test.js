3294efd4e4cd77da3e3226386de2547b5dd6b166
Mycroft1891 <mycroft1891@gmail.com>
2020-03-24 00:28:39 +0100

    Add: tests for the App component and change to class component

--- react/ecommerce-client/src/App.test.js
+++ react/ecommerce-client/src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
+import { Switch, Route } from 'react-router-dom';
 import App from './App';
+import Header from './components/header/header';
 
-test('renders learn react link', () => {
-  // const { getByText } = render(<App />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  let app;
+
+  beforeEach(() => {
+    app = shallow(<App />);
+  });
+
+  test('renders app component', () => {
+    expect(app.debug()).toMatchSnapshot();
+  });
+
+  test('currentUser is set to null', () => {
+    expect(app.state()).toEqual({ currentUser: null });
+  });
+
+  test('renders all sub-components', () => {
+    expect(app.find(Header).length).toEqual(1);
+    expect(app.find(Switch).length).toEqual(1);
+    expect(app.find(Route).length).toEqual(3);
+  });
 });
