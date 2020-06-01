9fad3256029e1a146d2cd0ca51686683defc8cde
Alexander Luna <alexanderluna1891@gmail.com>
2020-06-01 02:12:32 +0200

    🐛 Fix: CORS requests

--- express/graphql-authentication/client/src/index.tsx
+++ express/graphql-authentication/client/src/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import Routes from './Routes';
 import * as serviceWorker from './serviceWorker';
+import getTokenOrRedirect from './helper/auth';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+  credentials: 'include'
+})
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await getTokenOrRedirect();
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  };
+});
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
--- express/graphql-authentication/server/src/index.ts
+++ express/graphql-authentication/server/src/index.ts
@@ -7,46 +7,51 @@ import { UserResolver } from "./UserResolvers";
 import { createConnection } from "typeorm";
 import cookieParser from "cookie-parser";
 import { verify } from "jsonwebtoken";
+import cors from "cors";
 import { env } from "process";
 import { User } from "./entity/User";
 import { createAccessToken, createRefreshToken } from "./helper/userHelper";
 import { sendRefreshToken } from "./helper/indexHelper";
 
 (async () => {
-    const app = express();
-    app.use(cookieParser());
+  const app = express();
+  app.use(cookieParser());
+  app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+  }));
 
-    await createConnection();
+  await createConnection();
 
-    app.post('/refresh_token', async (req, res) => {
-        try {
-            const token = req.cookies.gqid;
-            const payload = verify(token, env.REFRESH_KEY!) as any;
-            const user = await User.findOne({ id: payload.id }) as User;
-            if (user.tokenVersion !== payload.tokenVersion) {
-                throw new Error('Invalid token')
-            }
-            sendRefreshToken(res, createRefreshToken(user));
-            return res.send({ ok: true, refreshToken: createAccessToken(user) });
-        } catch {
-            return res.send({ ok: false, accessToken: '' });
-        }
-    });
+  app.post('/refresh_token', async (req, res) => {
+    try {
+      const token = req.cookies.gqid;
+      const payload = verify(token, env.REFRESH_KEY!) as any;
+      const user = await User.findOne({ id: payload.id }) as User;
+      if (user.tokenVersion !== payload.tokenVersion) {
+        throw new Error('Invalid token')
+      }
+      sendRefreshToken(res, createRefreshToken(user));
+      return res.send({ ok: true, accessToken: createAccessToken(user) });
+    } catch {
+      return res.send({ ok: false, accessToken: '' });
+    }
+  });
 
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers: [UserResolver]
-        }),
-        context: ({ req, res }) => ({ req, res })
-    });
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [UserResolver]
+    }),
+    context: ({ req, res }) => ({ req, res })
+  });
 
-    apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({ app, cors: false });
 
-    app.listen(4000, () => {
-        console.log(`
+  app.listen(4000, () => {
+    console.log(`
         🚀 Running on port 4000
         🖥  Server: http://localhost:4000/
         🧪 GraphiQL: http://localhost:4000/graphql
         `);
-    });
+  });
 })();
