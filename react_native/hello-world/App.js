38725a19619ae1c9b873abf935d0552e73afc9a6
Alexander Luna <alexanderluna1891@gmail.com>
2020-06-07 01:57:16 +0200

    ⬆️  Add: expo project and upgrade from sdk33 to sdk39

---
+++ react_native/hello-world/App.js
@@ -0,0 +1,17 @@
+import 'react-native-gesture-handler';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import HomeScreen from './src/screens/HomeScreen';
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      title: 'App'
+    }
+  }
+);
+
+export default createAppContainer(navigator);
